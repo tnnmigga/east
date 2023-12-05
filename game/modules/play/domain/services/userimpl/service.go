98dfb11ef316c0492111d124010b1fd990b8cfbe
tangming <tangming@nibirutech.com>
2023-12-06 03:19:47 +0800

    1

---
+++ game/modules/play/domain/services/userimpl/service.go
@@ -0,0 +1,21 @@
+package userimpl
+
+import (
+	"east/game/modules/play/domain"
+	"east/game/modules/play/domain/api"
+)
+
+type service struct {
+	*domain.Domain
+}
+
+func New(d *domain.Domain) api.IMsg {
+	return &service{}
+}
+
+func (s *service) Init() {
+	s.regMsgHandler()
+}
+
+func (s *service) Destroy() {
+}
