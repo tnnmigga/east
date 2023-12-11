0bb991f87caf97caabf42d3601a013cd562dea19
tangming <tangming@nibirutech.com>
2023-12-12 00:29:58 +0800

    1

--- game/modules/play/domain/services/userimpl/register.go
+++ game/modules/play/domain/services/userimpl/register.go
@@ -8,17 +8,14 @@ import (
 func (s *service) register() {
 	s.registerHandler()
 	s.registerEvent()
-	s.registerTime()
 }
 
 func (s *service) registerHandler() {
 	msgbus.RegisterHandler(s, s.onSayHelloReq)
+	// timer
+	msgbus.RegisterHandler(s, s.onTimerSayHello)
 }
 
 func (s *service) registerEvent() {
 	s.EventImpl().RegisterHandler(define.EventUserSayHello, s.onEventUserMsg)
-}
-
-func (s *service) registerTime() {
-	msgbus.RegisterHandler(s, s.onTimerSayHello)
 }
