ca0abe54763f0341b73cca2402c2726e5a8a7bc7
tangming <tangming@nibirutech.com>
2024-04-10 15:49:40 +0800

    1

--- game/modules/play/domain/impl/player/handler.go
+++ game/modules/play/domain/impl/player/handler.go
@@ -5,8 +5,10 @@ import (
 	"east/pb"
 	"time"
 
+	"github.com/tnnmigga/nett/core"
 	"github.com/tnnmigga/nett/eventbus"
 	"github.com/tnnmigga/nett/msgbus"
+	"github.com/tnnmigga/nett/util"
 	"github.com/tnnmigga/nett/zlog"
 )
 
@@ -20,6 +22,20 @@ func (c *useCase) onSayHelloReq(msg *pb.SayHelloReq) {
 	c.TimerCase().New(time.Second*2, &timerSayHello{
 		UserID: 1,
 		Text:   "hello client!",
+	})
+	zlog.Infof("http get 1 %f", util.NowSec())
+	core.Async(c, func() ([]byte, error) {
+		zlog.Infof("http get 2 %f", util.NowSec())
+		res, err := util.HttpGet("https://www.baidu.com")
+		zlog.Infof("http get 3 %f", util.NowSec())
+		return res, err
+	}, func(b []byte, err error) {
+		if err != nil {
+			zlog.Errorf("http get error %v", err)
+			return
+		}
+		zlog.Infof("http get 4 %f", util.NowSec())
+		zlog.Infof("http get res %v", string(b))
 	})
 }
 
